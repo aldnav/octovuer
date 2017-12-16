0c1780bff413634e092d94d23d3a2ddbc924d585
Aldrin Navarro <aldrinnavarro16@gmail.com>
2017-12-16 19:35:05 +0800

    Complete exercise 1

--- exercise1.js
+++ exercise1.js
@@ -2,7 +2,8 @@ var app = new Vue({
     el: '#exercise',
     data: {
         name: 'Aldrin Navarro',
-        age: 23
+        age: 23,
+        imageUrl: 'http://doge2048.com/meta/doge-600.png'
     },
     methods: {
         getRandomNumber: (min=0, max=1) => {
