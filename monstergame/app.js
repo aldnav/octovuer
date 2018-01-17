fce2d275047db782241ecb7fd65c83ba6a21430b
Aldrin Navarro <aldrinnavarro16@gmail.com>
2018-01-17 15:18:18 +0800

    Add monster game

---
+++ monstergame/app.js
@@ -0,0 +1,56 @@
+new Vue({
+    el: '#game',
+    data: {
+        player1: {
+            name: 'You',
+            health: 100,
+            healing: 3
+        },
+        player2: {
+            name: 'Monster',
+            health: 100,
+            healing: 3
+        },
+    },
+    methods: {
+        startNewGame: function() {
+            this.player1.health = 100;
+            this.player1.healing = 3;
+            this.player2.health = 100;
+            this.player2.healing = 3;
+        },
+        attackMonster: function(minAttack, maxAttack) {
+            this.attack(this.player2, minAttack, maxAttack);
+            setTimeout(function() {
+                if (this.player2.health > 0) {
+                    this.attack(this.player1);
+                }
+            }.bind(this), 1000);
+        },
+        attack: function(opponent, minAttack=1, maxAttack=20) {
+            var damage = getRandomIntInclusive(minAttack, maxAttack);
+            opponent.health -= damage;
+            if (opponent.health <= 0) {
+                opponent.health = 0;
+            }
+        },
+        heal: function(player) {
+            if (player.healing > 0) {
+                player.health += getRandomIntInclusive(10, 20);
+                if (player.health > 100) {
+                    player.health = 100;
+                }
+                player.healing--;
+            }
+        },
+        quit: function() {
+
+        }
+    }
+});
+
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
