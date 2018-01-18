08183d71a358b06f47d56d2951d9d5a8ca232e91
Aldrin Navarro <aldrinnavarro16@gmail.com>
2018-01-18 19:50:01 +0800

    Add logs. Update rules.

--- monstergame/app.js
+++ monstergame/app.js
@@ -3,7 +3,7 @@ new Vue({
     data: {
         gameIsRunning: false,
         player1: {
-            name: 'You',
+            name: 'Player',
             health: 100,
             healing: 3
         },
@@ -12,6 +12,7 @@ new Vue({
             health: 100,
             healing: 3
         },
+        logs: []
     },
     methods: {
         startNewGame: function() {
@@ -20,6 +21,7 @@ new Vue({
             this.player2.health = 100;
             this.player2.healing = 3;
             this.gameIsRunning = true;
+            this.logs = [];
         },
         attackMonster: function(minAttack, maxAttack) {
             this.attack(this.player2, minAttack, maxAttack);
@@ -32,21 +34,42 @@ new Vue({
         attack: function(opponent, minAttack=1, maxAttack=20) {
             var damage = getRandomIntInclusive(minAttack, maxAttack);
             opponent.health -= damage;
+            var attacker = opponent.name === this.player2.name ? this.player1 : this.player2;
+            this.logs.unshift({
+                isPlayer: attacker.name === this.player1.name,
+                text: `${attacker.name} attacks by ${damage} damage!`
+            });
             if (opponent.health <= 0) {
                 opponent.health = 0;
             }
         },
         heal: function(player) {
             if (player.healing > 0) {
-                player.health += getRandomIntInclusive(10, 20);
+                var healPoints = getRandomIntInclusive(10, 20);
+                player.health += healPoints;
                 if (player.health > 100) {
                     player.health = 100;
                 }
+                this.logs.unshift({
+                    isPlayer: player.name === this.player1.name,
+                    text: `${player.name} heals by ${healPoints} points.`
+                });
                 player.healing--;
             }
         },
         quit: function() {
             this.gameIsRunning = false;
+        },
+        getLogClass: function(log) {
+            return [
+                `${log.isPlayer ? 'by-player': 'by-monster'}`,
+                `${log.text.indexOf('heal') >= 0 ? 'healing': ''}`
+            ];
+        }
+    },
+    computed: {
+        isGameOver: function() {
+            return this.player1.health === 0 || this.player2.health === 0;
         }
     }
 });
